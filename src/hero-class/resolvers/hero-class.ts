abb95ce98bfdc349199cd5f18cd5641660f03237
Adrian Popescu <adrian@gosugamers.net>
2022-05-11 09:39:59 +0200

    Add hero class mutations
    Implement create and delete hero classes functions.

--- src/hero-class/resolvers/hero-class.ts
+++ src/hero-class/resolvers/hero-class.ts
@@ -1,4 +1,4 @@
-import {Arg, Query, Resolver} from "type-graphql";
+import {Arg, Mutation, Query, Resolver} from "type-graphql";
 import {HeroClass} from "../models/hero-class";
 import {dataSource} from "../../app/data-source";
 
@@ -12,6 +12,22 @@ class HeroClassResolver {
     @Query(() => HeroClass)
     public async heroClass(@Arg("id") id: number): Promise<HeroClass | null> {
         return dataSource.manager.findOneBy(HeroClass, {id});
+    }
+
+    @Mutation(() => HeroClass)
+    public async create(
+        @Arg("name") name: string,
+        @Arg("imageUrl") imageUrl: string
+    ): Promise<HeroClass> {
+        return HeroClass.create({name, imageUrl}).save();
+    }
+
+    @Mutation(() => Boolean)
+    public async delete(@Arg("id") id: number): Promise<boolean> {
+        const heroClass = await HeroClass.findOneByOrFail({id});
+        await heroClass.remove();
+
+        return true;
     }
 }
 
